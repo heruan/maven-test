ab905956c33e50f6ca0b5daadf818b5a03604e52
Giovanni Lovato <giovanni@lova.to>
2024-01-25 15:22:11 +0100

    Create project

---
+++ src/test/java/io/github/heruan/AppTest.java
@@ -0,0 +1,13 @@
+package io.github.heruan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void alwaysTrue() {
+        assertTrue(true);
+    }
+}
